943b61d4f58e6acb9cc176219412f84543353569
Vaibhav Shrivastava <134196260+vaibhav9849@users.noreply.github.com>
2026-07-20 18:38:05 +0530

    Update axiosClient.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://fullstack-kanban-app-main.onrender.com//api/v1/'
+const baseUrl = 'https://fullstack-kanban-app-main.onrender.com/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
